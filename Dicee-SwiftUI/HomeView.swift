993083bd092052eb07fcb7c66a14aff34cdb64cc
m-zaink <sidsmith9@gmail.com>
2020-11-21 12:24:52 +0530

    Set up project | FC

--- Dicee-SwiftUI/HomeView.swift
+++ Dicee-SwiftUI/HomeView.swift
@@ -7,15 +7,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("Background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
