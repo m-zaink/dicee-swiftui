826aa4ebd7be8f5109238ddeb4e3574f55a1264d
m-zaink <sidsmith9@gmail.com>
2020-11-21 12:09:36 +0530

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Mohammed Sadiq on 21/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
